9bba167d659f0b82f5b7cf42c6967b1130d2845d
Cheng <chuachengcheng95@gmail.com>
2016-06-06 15:02:59 +1000

    Update loldatetime.py

--- loldatetime.py
+++ loldatetime.py
@@ -2,7 +2,7 @@ from datetime import datetime
 now = datetime.now()
 
 
-print 'what time is it'
+print 'what time is it?'
 print '%s:%s:%s %s/%s/%s' % (now.hour, now.minute, now.second, now.month, now.day, now.year)
 print 'time to get a watch'
 
