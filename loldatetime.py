218264aaa8bcc1018f6347c7c9aef1ac54f1e672
Cheng <chuachengcheng@macbkair.gateway>
2016-06-06 11:22:39 +1000

    hahaha

---
+++ loldatetime.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+now = datetime.now()
+
+
+print 'what time is it'
+print '%s:%s:%s %s/%s/%s' % (now.second, now.minute, now.hour, now.month, now.day, now.year)
+print 'time to get a watch'
+
+
+print 'ha ha ha'
